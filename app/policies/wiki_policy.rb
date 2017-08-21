5cc51f4616c42b6ab43c1fe2c4d68f4bc67a4d7f
Jamison Rubino <jamison.rubino@gmail.com>
2017-08-21 22:04:16 +0000

    Modified some aesthetic stuff, fixed a few errors.

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -11,23 +11,28 @@ class WikiPolicy < ApplicationPolicy
  
      def resolve
        wikis = []
-       if user.role == 'admin'
+       if @user && @user.role == 'admin'
          wikis = scope.all # if the user is an admin, show them all the wikis
-       elsif user.role == 'premium'
+       elsif @user && @user.role == 'premium'
          all_wikis = scope.all
          all_wikis.each do |wiki|
            if !wiki.private || wiki.user_id == user.id || wiki.users.pluck(:id).include?(user.id)
              wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
            end
          end
-       else # this is the lowly standard user
+       elsif @user && @user.role == 'standard' # this is the lowly standard user
          all_wikis = scope.all
-         wikis = []
          all_wikis.each do |wiki|
-           if !wiki.private || wiki.users.pluck(:id).include?(user.id)
-             puts wiki.users.pluck(:id)
-             wikis << wiki # only show standard users public wikis and private wikis they are a collaborator on
+           if !wiki.private || wiki.user_id == user.id || wiki.users.pluck(:id).include?(user.id)
+             wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
            end
+         end
+       else
+         all_wikis = scope.all
+         all_wikis.each do |wiki|
+            if !wiki.private
+               wikis << wiki
+            end
          end
        end
        wikis # return the wikis array we've built up
